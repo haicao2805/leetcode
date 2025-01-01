bae3646a268588f63b6606683a251b25339859c1
Cao Chi Hai <haicao2805@gmail.com>
2025-01-01 20:43:43 +0700

    add: medium 1658 minimum operations to reduce X to Zero

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-import { containsDuplicate } from "./easy/0217-contains-duplicate";
+import { minOperations } from "./medium/1658-minimun-operations-to-reduce-x-to-0";
 
-console.log(containsDuplicate([1, 2, 3, 1]));
-console.log(containsDuplicate([1, 2, 3, 4]));
-console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
+// console.log(minOperations([1, 1, 4, 2, 3], 5));
+// console.log(minOperations([5, 6, 7, 8, 9], 4));
+// console.log(minOperations([3, 2, 20, 1, 1, 3], 10));
+// console.log(minOperations([1, 1], 3));
+console.log(
+  minOperations(
+    [
+      8828, 9581, 49, 9818, 9974, 9869, 9991, 10000, 10000, 10000, 9999, 9993,
+      9904, 8819, 1231, 6309,
+    ],
+    134365,
+  ),
+);
