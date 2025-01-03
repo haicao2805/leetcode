266f5a9363a57074349a4b0f7f64a3b54a0ca348
Cao Chi Hai <haicao2805@gmail.com>
2025-01-03 18:40:44 +0700

    add: medium 2270 number of ways to split array

--- index.ts
+++ index.ts
@@ -1,15 +1,4 @@
-import { minOperations } from "./medium/1658-minimun-operations-to-reduce-x-to-0";
+import { waysToSplitArray } from "./medium/2270-number-of-ways-to-split-array";
 
-// console.log(minOperations([1, 1, 4, 2, 3], 5));
-// console.log(minOperations([5, 6, 7, 8, 9], 4));
-// console.log(minOperations([3, 2, 20, 1, 1, 3], 10));
-// console.log(minOperations([1, 1], 3));
-console.log(
-  minOperations(
-    [
-      8828, 9581, 49, 9818, 9974, 9869, 9991, 10000, 10000, 10000, 9999, 9993,
-      9904, 8819, 1231, 6309,
-    ],
-    134365,
-  ),
-);
+console.log(waysToSplitArray([10, 4, -8, 7]));
+console.log(waysToSplitArray([2, 3, 1, 0]));
