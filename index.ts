a3d94e74f75e684e49b196053dbd6d1f34e55a6f
Cao Chi Hai <haicao2805@gmail.com>
2025-01-05 12:34:31 +0700

    add: medium 2910 mininum number of groups to create a valid assignment

--- index.ts
+++ index.ts
@@ -1,4 +1,21 @@
-import { waysToSplitArray } from "./medium/2270-number-of-ways-to-split-array";
+import { minGroupsForValidAssignment } from "./medium/2910-minimun-number-of-groups-to-create-a-valid-assignment";
 
-console.log(waysToSplitArray([10, 4, -8, 7]));
-console.log(waysToSplitArray([2, 3, 1, 0]));
+console.log(minGroupsForValidAssignment([3, 2, 3, 2, 3]) === 2);
+console.log(minGroupsForValidAssignment([10, 10, 10, 3, 1, 1]) === 4);
+console.log(minGroupsForValidAssignment([2, 3, 2, 2, 2]) === 3);
+console.log(minGroupsForValidAssignment([3, 2, 2, 1, 1, 1, 2]) === 5);
+console.log(minGroupsForValidAssignment([1, 2, 3, 1, 1, 3, 1, 3]) === 5);
+console.log(
+  minGroupsForValidAssignment([1, 1, 3, 3, 1, 1, 2, 2, 3, 1, 3, 2]) === 5,
+);
+
+console.log(
+  minGroupsForValidAssignment([3, 1, 3, 2, 3, 2, 2, 1, 1, 2, 2, 2, 3, 2, 2]) ===
+    4,
+);
+
+console.log(
+  minGroupsForValidAssignment([
+    1, 1, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2, 3, 1, 2, 2, 2, 2,
+  ]) === 5,
+);
