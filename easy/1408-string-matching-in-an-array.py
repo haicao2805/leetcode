7bfe3b49177b598c216e24e441c4d8214ad54d85
Cao Chi Hai <haicao2805@gmail.com>
2025-01-07 21:21:52 +0700

    add: easy 1408 string mathing in an array

---
+++ easy/1408-string-matching-in-an-array.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res: List[str] = []
+        map = {}
+
+        for i in range(0, len(words)):
+            for j in range(0, len(words)):
+                if i != j and words[i] in words[j]:
+                    if not map.get(words[i]):
+                        map[words[i]] = True
+                        res.append(words[i])
+
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.stringMatching(["mass","as","hero","superhero"]))
+    print(s.stringMatching(["leetcoder","leetcode","od","hamlet","am"]))
