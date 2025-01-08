1c42f02c99521f99377dc698d36066b8303e4d08
Cao Chi Hai <haicao2805@gmail.com>
2025-01-08 19:45:58 +0700

    add: easy 3042 count prefix and suffix pairs 1

---
+++ easy/3042-count-prefix-and-suffix-pairs-1.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def isPrefixAndSuffix(self, str1: str, str2: str) -> int:
+        if len(str1) > len(str2):
+            return 0
+
+        if str1 == str2:
+            return 1
+
+        prefix = str2[0:len(str1)]
+        if prefix != str1:
+            return 0
+
+        suffix = str2[-len(str1):]
+        if suffix != str1:
+            return 0
+
+        if prefix == suffix:
+            return 1
+
+        return 0
+
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        res = 0
+        for i in range(0, len(words) - 1):
+            for j in range(i + 1, len(words)):
+                res += self.isPrefixAndSuffix(words[i], words[j])
+        return res
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.countPrefixSuffixPairs(["a","aba","ababa","aa"]))
+    print(s.countPrefixSuffixPairs(["pa","papa","ma","mama"]))
