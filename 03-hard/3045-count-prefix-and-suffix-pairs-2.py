824c33848fd925b1fe7ccd782db8da762819a719
Cao Chi Hai <haicao2805@gmail.com>
2025-01-11 18:30:56 +0700

    add: hard 3045 count prefix and suffix pairs 2

--- 03-hard/3045-count-prefix-and-suffix-pairs-2.py
+++ 03-hard/3045-count-prefix-and-suffix-pairs-2.py
@@ -1,41 +1,34 @@
 from typing import List
 
 class Solution:
-    def getHash(self, str) -> int:
-        hash = 0
-        for i in range(len(str)):
-            hash += (ord(str[i]) - 97) * 37 ** (i + 1) % 1_000_000_001
-        return hash
-
-
-    def isPrefixAndSuffix(self, str1: str, str2: str) -> int:
-        if len(str1) > len(str2):
-            return 0
-
-        hashStr1 = self.getHash(str1)
-        # print(str1, hashStr1)
-        hashPrefix = self.getHash(str2[0:len(str1)])
-        # print(str2[0:len(str1)], hashPrefix)
-        hashSuffix = self.getHash(str2[-len(str1):])
-        # print(str2[-len(str1):], hashSuffix)
-
-        if hashStr1 == hashPrefix and hashStr1 == hashSuffix:
-            print(str1, hashStr1)
-            print(str2[0:len(str1)], hashPrefix)
-            print(str2[-len(str1):], hashSuffix)
-            return 1
-
-        return 0
-
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        base = 37
+        m = 1_000_000_007
+        hmap = {}
         res = 0
-        for i in range(0, len(words) - 1):
-            for j in range(i + 1, len(words)):
-                res += self.isPrefixAndSuffix(words[i], words[j])
+
+        for w in words:
+            prefix = 0
+            suffix = 0
+            power = 1
+
+            for i in range(len(w)):
+                prefix = (prefix * base + (ord(w[i]) - ord('a') + 1)) % m
+                suffix = (suffix + power * (ord(w[len(w) - i - 1]) - ord('a') + 1)) % m
+                power = (power * base) % m
+
+                if prefix == suffix:
+                    res += hmap.get(suffix, 0)
+
+            hmap[suffix] = hmap.get(suffix, 0) + 1
+
         return res
 
 if __name__ == "__main__":
     s = Solution()
-    print(s.countPrefixSuffixPairs(["acc","acb","ac","ca","acb"]) == 1)
-    # print(s.countPrefixSuffixPairs(["pa","papa","ma","mama"]) == 2)
-    # print(s.countPrefixSuffixPairs(["abab","ab"]) == 0)
+    print(s.countPrefixSuffixPairs(["bba","aacbc","bbba","cabb","aa","aaccc","aa","bba","c"]))
+    print(s.countPrefixSuffixPairs(["a","aba","ababa","aa"]))
+    print(s.countPrefixSuffixPairs(["pa","papa","ma","mama"]))
+    print(s.countPrefixSuffixPairs(["abab","ab"]))
+    print(s.countPrefixSuffixPairs(["b","a","b","a","b"]))
+    print(s.countPrefixSuffixPairs(["c","ccbac","bc","cc","ba","acbbc","aacbc","ccbbb","bca","bcbbb","abbaa","bbaa","bbb"]))
