9eea797f8b9ef56c33348b27c8a12ef4c4373b7a
Cao Chi Hai <haicao2805@gmail.com>
2025-01-13 13:24:32 +0700

    add: medium 3223 minimum length of string after operations

---
+++ 02-medium/3223-minimun-length-of-string-after-operations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        res = len(s)
+        hmap = {}
+        for c in s:
+            hmap[c] = hmap.get(c, 0) + 1
+
+        for v in hmap.values():
+            if v > 2:
+                res -= v - 1 if v % 2 == 1 else v - 2
+
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.minimumLength("abaacbcbb") == 5)
+    print(s.minimumLength("aa") == 2)
+    print(s.minimumLength("aaabbbbccccc") == 4)
