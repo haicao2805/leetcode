f5f2c759615fcf9252de6bdde8f45df3ea16afec
Cao Chi Hai <haicao2805@gmail.com>
2025-01-19 19:57:36 +0700

    add: easy 1009 complement of base 10 integer

---
+++ 01-easy/1009-complement-of-base-10-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def bitwiseComplement(self, n: int) -> int:
+        res = 0
+        idx = 0
+
+        if n == 0:
+            return 1
+
+        while n:
+            res += pow(2, idx) if n & 1 == 0 else 0
+            idx += 1
+            n >>= 1
+
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.bitwiseComplement(5))
+    print(s.bitwiseComplement(10))
