ed6c6643e4a039b743f867a55776c625440c1a5e
Cao Chi Hai <haicao2805@gmail.com>
2025-01-19 19:46:09 +0700

    add: easy 0136 single number

---
+++ 01-easy/0136-single-number.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+
+        for n in nums:
+            res ^= n
+
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.singleNumber([2,2,1]))
+    print(s.singleNumber([4,1,2,1,2]))
+    print(s.singleNumber([1]))
