98b6bc5ae3b77d2aba2763edaa43a779cc337e48
Cao Chi Hai <haicao2805@gmail.com>
2025-01-21 20:49:49 +0700

    add: easy 0191 number of 1 bits

---
+++ bit-manipulation/01-easy/0191-number-of-1-bits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n:
+            res += n & 1
+            n >>= 1
+
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.hammingWeight(11))
+    print(s.hammingWeight(128))
+    print(s.hammingWeight(2147483645))
