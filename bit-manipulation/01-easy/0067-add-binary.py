0ab35fc9d3df6c038793414f0fb50baf98c79aef
Cao Chi Hai <haicao2805@gmail.com>
2025-01-22 22:07:42 +0700

    add: easy 0067 add binary

---
+++ bit-manipulation/01-easy/0067-add-binary.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0:
+            sum = carry
+            if i >= 0:
+                sum += ord(a[i]) - ord('0')
+            if j >= 0:
+                sum += ord(b[j]) - ord('0')
+            i, j = i - 1, j - 1
+            carry = 1 if sum > 1 else 0
+            res += str(sum % 2)
+
+        if carry != 0 : res += str(carry)
+        return res[::-1]
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.addBinary("11", "1"))
+    print(s.addBinary("1011", "101"))
+    print(s.addBinary("1010", "1011"))
