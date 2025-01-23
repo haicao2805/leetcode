598497c18c4230823a127a3f57d093dce4ba86b0
Cao Chi Hai <haicao2805@gmail.com>
2025-01-23 21:19:48 +0700

    adjust: bit manipulate solution

--- bit-manipulation/01-easy/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ bit-manipulation/01-easy/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -3,6 +3,17 @@ class Solution:
         res = 0
         while num:
             res += 1
+            if num & 1:
+                num ^= 1
+            else:
+                num >>= 1
+        return res
+
+class SimpleSolution:
+    def numberOfSteps(self, num: int) -> int:
+        res = 0
+        while num:
+            res += 1
             if num % 2 == 0:
                 num = int(num / 2)
             else: num -= 1
