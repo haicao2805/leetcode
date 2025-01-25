c6d71d241eb507b25109e4731a600d7fdf4ab787
Cao Chi Hai <haicao2805@gmail.com>
2025-01-25 08:14:36 +0700

    add: easy 0231 power of two

---
+++ bit-manipulation/01-easy/0231-power-of-two.py
@@ -0,0 +1,9 @@
+import math
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+        return int(math.log2(n)) == math.log2(n)
