e08fe1b4474cc16012dad986d93d5218bbc91174
Cao Chi Hai <haicao2805@gmail.com>
2025-01-25 08:15:25 +0700

    add: easy 0342 power of four

---
+++ bit-manipulation/01-easy/0342-power-of-four.py
@@ -0,0 +1,14 @@
+import math
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+
+        sqrtN = math.sqrt(n)
+
+        log2SqrtN = math.log2(sqrtN)
+
+        return log2SqrtN == int(log2SqrtN)
