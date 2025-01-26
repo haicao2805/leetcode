a0a72e04ffdad5439a7c5f1dda7539f58837cf89
Cao Chi Hai <haicao2805@gmail.com>
2025-01-26 11:05:16 +0700

    add: easy 3158 find the xor of numbers which appear twice

---
+++ bit-manipulation/01-easy/3158-find-the-xor-of-numbers-which-appear-twice.py
@@ -0,0 +1,26 @@
+from typing import List
+
+# Sort
+class SortSolution:
+    def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        res = 0
+
+        sortedNums = sorted(nums)
+
+        for i in range(1,len(nums)):
+            if sortedNums[i] ^ sortedNums[i - 1] == 0:
+                res ^= sortedNums[i]
+
+        return res
+
+
+# Hashmap
+class HashmapSolution:
+    def duplicateNumbersXOR(self, nums: List[int]) -> int:
+        hmap = {}
+        res = 0
+        for n in nums:
+            if n in hmap:
+                res ^= n
+            hmap[n] = 1
+        return res
