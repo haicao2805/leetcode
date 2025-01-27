a9d967c30df3d5e5af1d4c2b6884ad99c9ec6860
Cao Chi Hai <haicao2805@gmail.com>
2025-01-27 09:11:06 +0700

    add: 0028 find the index of first occurence in a string

---
+++ string-matching/01-easy/0028-find-the-index-of-first-occurrence-in-a-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack) - len(needle) + 1):
+            if haystack[i] == needle[0]:
+                idx = 0
+                while idx < len(needle) and haystack[i+idx] == needle[idx]:
+                    idx += 1
+
+                if idx == len(needle):
+                    return i
+
+        return -1
