cd97f438f7c310f9fcbe2dfd9f3edb80f7f29b9e
Cao Chi Hai <haicao2805@gmail.com>
2025-01-30 12:40:10 +0700

    add: easy 0724 find pivot index

---
+++ 01-easy/0724-find-pivot-index.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        N = len(nums)
+        preSum = [0 for _ in range(N + 1)]
+
+        for i in range(N):
+            preSum[i + 1] = preSum[i] + nums[i]
+
+        for i in range(N):
+            if preSum[i] == preSum[-1] - preSum[i + 1]:
+                return i
+
+        return -1
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.pivotIndex([2,1,-1]))
