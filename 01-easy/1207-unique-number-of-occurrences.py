5362661eb79c081086d8a1362733cdb88cebb2cf
Cao Chi Hai <haicao2805@gmail.com>
2025-01-30 12:40:34 +0700

    add: easy 1207 unique number of occurrences

---
+++ 01-easy/1207-unique-number-of-occurrences.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hmap = {}
+        occurSet = set()
+        for n in arr:
+            if n not in hmap:
+                hmap[n] = 1
+            else:
+                hmap[n] += 1
+
+        for v in hmap.values():
+            if v in occurSet:
+                return False
+            occurSet.add(v)
+        return True
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.uniqueOccurrences([1,2,2,1,1,3]))
