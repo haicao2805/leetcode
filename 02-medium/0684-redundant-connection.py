c88103becc41111e0bd28049e75eb7208ff866f6
Cao Chi Hai <haicao2805@gmail.com>
2025-01-31 11:10:01 +0700

    add: medium 0684 redundant connection

---
+++ 02-medium/0684-redundant-connection.py
@@ -0,0 +1,48 @@
+from collections import deque
+from typing import List
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        N = len(edges)
+        adj: dict[int, set[int]] = {}
+
+        def bfs(start: int, visit: List[bool]):
+            q = deque()
+            q.append((start, 0))
+
+            while q:
+                (node, parent) = q.popleft()
+                visit[node] = True
+
+                for nei in adj.get(node, []):
+                    if visit[nei] and nei != parent:
+                        return True
+                    if nei == parent:
+                        continue
+
+                    visit[nei] = True
+                    q.append((nei, node))
+
+        for [u, v] in edges:
+            if u not in adj:
+                adj[u] = set()
+                adj[u].add(v)
+            else:
+                adj[u].add(v)
+
+            if v not in adj:
+                adj[v] = set()
+                adj[v].add(u)
+            else:
+                adj[v].add(u)
+
+            visit = [False for _ in range(N + 1)]
+            if bfs(u, visit):
+                return [u, v]
+
+        return []
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.findRedundantConnection([[1,2],[2,3],[3,4],[1,4],[1,5]]))
+    print(s.findRedundantConnection([[1,2],[1,3],[2,3]]))
