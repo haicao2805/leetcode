91f190b756deb49f85ea640b91b9c0aef7a66cc2
Cao Chi Hai <haicao2805@gmail.com>
2025-02-04 08:59:43 +0700

    add: easy 1800 maximum ascending subarray sum

---
+++ 01-easy/1800-maximum-ascending-subarray-sum.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1, n):
+            if nums[i] > nums[i - 1]:
+                dp[i] = dp[i - 1] + nums[i]
+            else:
+                dp[i] = nums[i]
+        return max(dp)
