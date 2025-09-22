9cc72c314599d7549a980ff94e2c34a127c41f94
Cao Chi Hai <haicao2805@gmail.com>
2025-09-22 11:39:49 +0700

    refactor: 01-easy/3005-count-elements-with-maximum-frequency.py

--- 01-easy/3005-count-elements-with-maximum-frequency.py
+++ 01-easy/3005-count-elements-with-maximum-frequency.py
@@ -1,7 +1,24 @@
-from typing import List
+from typing import Counter, List
 
 
 class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        freqMap = Counter(nums)
+        maxFreq = freqMap.most_common(1)[0][1]
+        return sum(freq for freq in freqMap.values() if freq == maxFreq)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxFrequencyElements([1, 2, 2, 3, 1, 4]))
+    print(s.maxFrequencyElements([1, 2, 3, 4, 5]))
+    print(s.maxFrequencyElements([10, 12, 11, 9, 6, 19, 11]))
+
+
+class VersionOneSolution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         numMap = {}
         for n in nums:
@@ -13,10 +30,3 @@ class Solution:
             if v == maxOccurrence:
                 res += v
         return res
-
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.maxFrequencyElements([1, 2, 2, 3, 1, 4]))
-    print(s.maxFrequencyElements([1, 2, 3, 4, 5]))
-    print(s.maxFrequencyElements([10, 12, 11, 9, 6, 19, 11]))
